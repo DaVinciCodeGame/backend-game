23090bc73b2032952fc68f6f7490a2fd0d89f041
GogumaYummy <sora1211x@gmail.com>
2023-01-17 02:14:24 +0900

    feat: 레디스 설정 추가

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,12 +3,14 @@ import Joi from 'joi';
 type Env = {
   PORT: string;
   NODE_ENV: string;
+  REDIS_URL: string;
 };
 
 const { value: env, error } = Joi.object<Env>()
   .keys({
     PORT: Joi.string(),
     NODE_ENV: Joi.string(),
+    REDIS_URL: Joi.string(),
   })
   .unknown()
   .validate(process.env);
