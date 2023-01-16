9cb91955aae63606b0d3b0420d1a2338acf0be8e
GogumaYummy <sora1211x@gmail.com>
2023-01-17 02:14:36 +0900

    feat: 소켓io 설정, 서버 진입점 추가

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
 import env from './config/env';
+import logger from './config/logger';
+import redis from './config/redis';
+import SocketIO from './socket-io';
+
+redis.connect().then(() => {
+  logger.info('Redis 연결 완료');
+  const socketIO = new SocketIO();
+
+  socketIO.listen(Number(env.PORT));
+});
