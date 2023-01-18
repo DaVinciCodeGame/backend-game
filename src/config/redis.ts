b4174e5cfd6ceabed9877fb48799f1d85cc1390e
GogumaYummy <sora1211x@gmail.com>
2023-01-18 20:17:17 +0900

    fix: 레디스 연결 최대 시간 설정

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -3,7 +3,7 @@ import Redis from 'ioredis';
 import env from './env';
 import logger from './logger';
 
-const redis = new Redis(env.REDIS_URL);
+const redis = new Redis(env.REDIS_URL, { commandTimeout: 10000 });
 
 redis.on('error', (err) => {
   logger.error(err);
