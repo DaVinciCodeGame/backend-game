b0dc6e33230293309bddf997b6ef06872e1c7264
GogumaYummy <sora1211x@gmail.com>
2023-01-18 17:09:37 +0900

    feat: 소켓 연결, 방 참여 구현

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import env from './config/env';
 import logger from './config/logger';
 import redis from './config/redis';
-import SocketIO from './socket-io';
+import io from './socket-io';
 
 redis.connect(() => {
   logger.info('Redis 연결 완료');
-  const socketIO = new SocketIO(redis);
 
-  socketIO.listen(Number(env.PORT));
+  io.listen(Number(env.PORT));
 });
--- src/socket-io.ts
+++ src/socket-io.ts
@@ -1,8 +1,9 @@
-import Redis from 'ioredis';
 import { Server, Socket } from 'socket.io';
 import Joi, { ValidationError } from 'joi';
 import events from './utils/event-names';
 import logger from './config/logger';
+import redis from './config/redis';
+import User from './utils/user';
 
 const serverOption = {
   path: '/game/',
@@ -19,54 +20,127 @@ const userDataSchema = Joi.object<{ userId: number; username: string }>()
   })
   .unknown();
 
-export default class SocketIO extends Server {
-  redis: Redis;
+const io = new Server(serverOption);
 
-  constructor(redis: Redis) {
-    super(serverOption);
+io.on(events.CONNECT, async (socket: Socket) => {
+  try {
+    // HACK: 게임 로직 완성 후 쿠키로 유저 정보 받아오게 바꿔야 함
+    const me = await userDataSchema.validateAsync(socket.handshake.query);
 
-    this.redis = redis;
+    // NOTE: 디버깅용
+    socket.onAny((event, ...args) => {
+      logger.debug({ type: 'input', event, args });
+    });
+    socket.onAnyOutgoing((event, ...args) => {
+      logger.debug({ type: 'output', event, args });
+    });
 
-    this.setListeners();
-  }
+    const existUser = await redis.hgetall(`users:${me.userId}`);
 
-  private setListeners() {
-    this.on(events.CONNECT, async (socket: Socket) => {
-      try {
-        // HACK: 게임 로직 완성 후 쿠키로 유저 정보 받아오게 바꿔야 함
-        const { userId, username } = await userDataSchema.validateAsync(
-          socket.handshake.query
-        );
+    if (!existUser) {
+      await redis.hmset(`users:${me.userId}`, {
+        userId: me.userId,
+        username: me.username,
+        socketId: socket.id,
+      });
+    } else {
+      io.to(existUser.socketId).disconnectSockets();
+      await redis.hset(`users:${me.userId}`, { socketId: socket.id });
+    }
 
-        // NOTE: 디버깅용
-        socket.onAny((event, ...args) => {
-          logger.debug(`들어온 이벤트: ${event}`);
-          logger.debug(`  ${args}`);
-        });
-        socket.onAnyOutgoing((event, ...args) => {
-          logger.debug(`나가는 이벤트: ${event}`);
-          logger.debug(`  ${args}`);
-        });
+    io.to(socket.id).emit(events.READY_TO_JOIN);
 
-        await this.redis.hset(`users/${userId}`, {
-          userId,
-          username,
-          socketId: socket.id,
-        });
+    socket.on(events.JOIN, async ({ roomId }: { roomId: number }) => {
+      const isPlaying = await redis.get(`rooms:${roomId}:is-playing`);
+      if (isPlaying) {
+        const existUsers = await redis.hgetall(`rooms:${roomId}:users`);
 
-        const user = await this.redis.hgetall(`users/${userId}`);
+        if (
+          JSON.parse(existUsers[0]).userId === me.userId ||
+          JSON.parse(existUsers[1]).userId === me.userId ||
+          JSON.parse(existUsers[2]).userId === me.userId ||
+          JSON.parse(existUsers[3]).userId === me.userId
+        ) {
+          // 게임이 진행 중이고 목록에 유저가 있을 때
+          socket.join(roomId.toString());
 
-        logger.debug(user);
+          const users = await redis.hgetall(`rooms:${roomId}:users`);
 
-        socket.on(events.DISCONNECT, async () => {
-          logger.debug(`${socket.id} 연결 해제`);
-        });
-      } catch (err) {
-        if (err instanceof ValidationError) {
-          this.to(socket.id).emit(events.ERROR, [0, err.message]);
-          socket.disconnect(true);
+          io.to(socket.id).emit(events.YOU_JOINED, { users });
+          socket.to(roomId.toString()).emit(events.NEW_USER_JOINED, { users });
+        }
+        // TODO: 게임이 진행 중이지만 목록에 유저가 없을 때
+        else {
+          io.to(socket.id).emit(events.ERROR, { code: 0, message: '' });
+        }
+      } else {
+        const numberOfExistUsers = await redis.hlen(`rooms:${roomId}:users`);
+
+        if (numberOfExistUsers > 3) {
+          // TODO: 다 찼을 때
+          io.to(socket.id).emit(events.ERROR, { code: 0, message: '' });
+        } else {
+          const existUsers = await redis.hgetall(`rooms:${roomId}:users`);
+
+          const pipeline = redis
+            .pipeline()
+            .hset(`users:${me.userId}`, { roomId });
+
+          if (!existUsers[0])
+            await pipeline
+              .hset(`rooms:${roomId}:users`, {
+                0: new User(me.userId, me.username, socket.id),
+              })
+              .exec();
+          else if (!existUsers[1])
+            await pipeline
+              .hset(`rooms:${roomId}:users`, {
+                1: new User(me.userId, me.username, socket.id),
+              })
+              .exec();
+          else if (!existUsers[2])
+            await pipeline
+              .hset(`rooms:${roomId}:users`, {
+                2: new User(me.userId, me.username, socket.id),
+              })
+              .exec();
+          else if (!existUsers[3])
+            await pipeline
+              .hset(`rooms:${roomId}:users`, {
+                3: new User(me.userId, me.username, socket.id),
+              })
+              .exec();
+
+          await socket.join(roomId.toString());
+
+          const users = await redis.hgetall(`rooms:${roomId}:users`);
+
+          io.to(socket.id).emit(events.YOU_JOINED, { users });
+          socket.to(roomId.toString()).emit(events.NEW_USER_JOINED, { users });
         }
       }
     });
+
+    socket.on(events.DISCONNECT, async () => {
+      logger.debug(`${socket.id} 연결 해제`);
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      logger.error(err.stack);
+      if (err instanceof ValidationError) {
+        io.to(socket.id).emit(events.ERROR, {
+          code: 0,
+          message: err.message,
+        });
+        socket.disconnect(true);
+      } else {
+        io.to(socket.id).emit(events.ERROR, {
+          code: 1,
+          message: '알 수 없는 에러',
+        });
+      }
+    }
   }
-}
+});
+
+export default io;
