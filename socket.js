9ba278fe59ff1f6a5adacaa63f5e1d204a144d2c
GogumaYummy <sora1211x@gmail.com>
2023-01-31 23:08:41 +0900

    fix: 테이블 생성 조건 삭제

--- socket.js
+++ socket.js
@@ -15,15 +15,13 @@ const { json } = require('sequelize');
 const e = require('express');
 const { table } = require('console');
 
-// db 연결 확인
-if (process.env.NODE_ENV === 'development') {
-  DB.sequelize
-    .sync({ force: false })
-    .then(() => {
-      console.log('database 연결 성공');
-    })
-    .catch(console.error);
-}
+// 테이블 생성
+DB.sequelize
+  .sync()
+  .then(() => {
+    console.log('database 연결 성공');
+  })
+  .catch(console.error);
 
 const io = new Server(server, {
   cors: {
