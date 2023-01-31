e8892d385843a2a5282359d1e676e202364977d8
GogumaYummy <sora1211x@gmail.com>
2023-01-31 23:11:46 +0900

    feat: 미들웨어 추가

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const routes = require('./routes');
+const helmet = require('helmet');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
+
+const routes = require('./routes');
+const errorHandler = require('./middlewares/error-handler');
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+app.use(errorHandler);
 
 module.exports = app;
