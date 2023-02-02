3535d34097da5e39185413dd3aa6713e34519ff9
GogumaYummy <sora1211x@gmail.com>
2023-02-02 21:57:42 +0900

    feat: 방 확인 결과 클래스 추가

---
+++ utils/room-check-result.js
@@ -0,0 +1,13 @@
+class Result {
+  /**
+   *
+   * @param {number} code
+   * @param {string} message
+   */
+  constructor(code, message) {
+    this.code = code;
+    this.message = message;
+  }
+}
+
+module.exports = Result;
