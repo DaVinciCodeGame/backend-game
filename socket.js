fc2c8fdfc4bae9911c89da72f838c5b517299ee3
yesk31 <yesk31@naver.com>
2023-02-03 17:14:08 +0900

    test: console test

--- socket.js
+++ socket.js
@@ -1073,8 +1073,8 @@ io.on('connection', async (socket) => {
 
     for (let i = 0; i < users.length; i++) {
       if (users[i].userId === tableInfo.turn) {
-        for (let j = 1; j < 4; j++) {
-          for (let z = 0; z < 4; z++) {
+        for (let j = 1; j < player.length; j++) {
+          for (let z = 0; z < player.length; z++) {
             console.log('player[0]', player[0].userId);
             console.log('player[1]', player[1].userId);
             if (users[(i + j) % users.length].userId == player[z].userId) {
