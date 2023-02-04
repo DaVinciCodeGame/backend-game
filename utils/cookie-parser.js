40e6469917834eb67c5c58856dfce8dbc7f9947e
GogumaYummy <sora1211x@gmail.com>
2023-02-04 12:30:53 +0900

    refactor: undefined를 반환하는 대신에 에러를 던지도록 변경

--- utils/cookie-parser.js
+++ utils/cookie-parser.js
@@ -5,11 +5,13 @@
  */
 function cookieParser(rawCookies) {
   if (!rawCookies || typeof rawCookies !== 'string') {
-    return undefined;
+    throw new Error('잘못된 쿠키 형식');
   }
 
   const parsedCookies = rawCookies.split('; ').reduce((cookies, rawCookie) => {
     const [name, value] = rawCookie.split('=');
+
+    if (!value) throw new Error('잘못된 쿠키 형식');
 
     return Object.assign(cookies, { [name]: value });
   }, {});
